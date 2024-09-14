c0c3f19856bb1225370ed59a139aab3e9930c641
Luiza Asth <168010169+luxnfrj@users.noreply.github.com>
2024-09-14 18:26:20 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,24 +7,31 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-    int codigoDaCidade, pontosTuristicos, populacao;
-    char nome[4];
+    int pontosTuristicos, populacao;
+    char codigoEstado;
+    char codigoCidade[3];
+    char nome[50];
     float area, PIB;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf("Digite o código da cidade: \n");
-    scanf("%d", &codigoDaCidade);
+    printf("CADASTRE SUA CARTA\n");
+
+    printf("Digite o código do Estado (A-H): \n");
+    scanf(" %c", &codigoEstado);
+
+    printf("Digite o código da cidade (A-H + 01-04): \n");
+    scanf("%s", codigoCidade);
 
     printf("Digite o nome da cidade: \n");
-    scanf("%s", &nome);
+    scanf("%s", nome);
 
     printf("Digite a população da cidade: \n");
     scanf("%d", &populacao);
 
-    printf("Digite a área da cidade: \n");
+    printf("Digite a área da cidade (em km2): \n");
     scanf("%f", &area);
 
     printf("Digite o PIB da cidade: \n");
@@ -37,7 +44,9 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("O código da cidade é: %d\n", codigoDaCidade);
+    printf("-------------------------- \n");
+    printf("O código do estado  é: %c\n", codigoEstado);
+    printf("O código da cidade é: %s\n", codigoCidade);
     printf("O nome da cidade é: %s\n", nome);
     printf("A população da cidade é: %d\n", populacao);
     printf("A área da cidade é: %f\n", area);
